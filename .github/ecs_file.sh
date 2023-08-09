d9481467ec3a6b6b80d82f947efbbc7430d9d1de
Tinashe <tinashemadangure@gmail.com>
2023-08-09 17:49:08 +0100

    updated ecs_file.sh

--- .github/ecs_file.sh
+++ .github/ecs_file.sh
@@ -16,4 +16,10 @@ task_definition_arn=$(echo $ecs_task_definition | jq -r '.taskDefinition.taskDef
 # Create ECS service
 aws ecs create-service --cluster $ECS_CLUSTER_NAME --service-name $ECS_SERVICE_NAME --task-definition $task_definition_arn --desired-count 1 --launch-type EC2 --region $AWS_REGION
 
+# Wait for the service to become stable
+until [ "$(aws ecs describe-services --cluster $ECS_CLUSTER_NAME --services $ECS_SERVICE_NAME --query 'services[0].status' --output text --region $AWS_REGION)" == "ACTIVE" ]; do
+    echo "Waiting for service to become stable..."
+    sleep 10
+done
 
+echo "Service is now stable."
