cc341bb3361402fea838c10955a07843da45e5b9
Tinashe <tinashemadangure@gmail.com>
2024-01-25 23:39:51 +0000

    updated tf

--- infrastructure.tf
+++ infrastructure.tf
@@ -10,70 +10,35 @@ resource "aws_ecs_cluster" "gaming-cluster" {
   name = "gaming-cluster"
 }
 
-# Read the ECS task definition JSON file
-data "template_file" "ecs_task_definition" {
-  template = file("${path.module}/.github/workflows/task_defination.json")
-}
-
-resource "aws_ecs_task_definition" "game-app-service" {
+resource "aws_ecs_task_definition" "my_task" {
   family                   = "gaming-Td"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
 
-  cpu                      = 256  # Specify the CPU at the task level
-  memory                   = 512  # Adjust memory as needed
-
-  container_definitions = jsonencode([
-    {
-      "name"            = "gaming",
-      "image"           = "954354975057.dkr.ecr.us-east-1.amazonaws.com/gaming",
-      "cpu"             = 0,  # Remove this line from the container definition
-      "memory"          = 512,
-      "portMappings"    = [
-        {
-          "name"          = "gaming-80-tcp",
-          "containerPort" = 80,
-          "hostPort"      = 80,
-          "protocol"      = "tcp",
-          "appProtocol"   = "http",
-        },
-      ],
-      "essential"       = true,
-      "environment"     = [],
-      "environmentFiles" = [],
-      "mountPoints"     = [],
-      "volumesFrom"     = [],
-      "ulimits"         = [],
-      "logConfiguration" = {
-        "logDriver" = "awslogs",
-        "options"   = {
-          "awslogs-create-group"       = "true",
-          "awslogs-group"              = "/ecs/gaming-Td",
-          "awslogs-region"             = "us-east-1",
-          "awslogs-stream-prefix"      = "ecs",
-        },
-        "secretOptions" = [],
-      },
-    },
-  ])
-
-  # Additional attributes (if needed)
   execution_role_arn = "arn:aws:iam::954354975057:role/GitHub-demo"
-  task_role_arn      = "arn:aws:iam::954354975057:role/GitHub-demo"
-  tags               = {}  # Add tags as needed
+
+  container_definitions = jsondecode(file(".github/workflows/task_defination.json"))
 }
 
-
-resource "aws_ecs_service" "game-app-service" {
-  name            = "game-app-service"
-  cluster         = aws_ecs_cluster.gaming-cluster.id
-  task_definition = aws_ecs_task_definition.game-app-service.arn
-  launch_type     = "FARGATE"  # Update if using EC2 launch type
+resource "aws_ecs_service" "my_service" {
+  name            = "your-service-name"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task.arn
+  launch_type     = "FARGATE"
 
   network_configuration {
-    subnets = ["subnet-0340f1a6cd80d7608", "subnet-0340f1a6cd80d7608"]  # Replace with your subnet IDs
-    security_groups = ["sg-07c4365745fc7f6ef"]  # Replace with your security group IDss
+    subnets = ["subnet-0340f1a6cd80d7608", "subnet-03ca82457d7c30b25"]
+    security_groups = ["sg-07c4365745fc7f6ef"]
   }
+
+  desired_count = 1
+  force_new_deployment = true  # Update this value if you want to force a new deployment on updates
 }
+
+output "public_ip" {
+  value = aws_ecs_service.my_service.tasks[0].network_interfaces[0].association[0].public_ip
+}
+
+
 
 
